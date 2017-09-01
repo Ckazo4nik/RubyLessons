f7c4b14c47e131cdd668c71d6b8dcdb5da76093d
Orest Kostiuk <xxmolekula@gmail.com>
2017-09-01 14:31:46 +0300

    Perfectweight

--- Perfectweight.rb
+++ Perfectweight.rb
@@ -1,1 +1,10 @@
-*.idea/
+puts "Ваше ім'я?"
+name = gets.chomp
+puts "Ваш ріст?"
+height = gets.chomp
+
+weight = height.to_i - 110
+puts "#{name}, ваша вага #{weight}"
+if weight < 0
+  puts "Ваш вес уже оптимальный"
+end
